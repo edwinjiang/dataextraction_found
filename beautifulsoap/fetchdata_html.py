bc6edc058eeb725b20e8e352562fc978d8f11489
edwin <986806345@qq.com>
2017-09-07 15:59:55 +0800

    fifthe commit

--- beautifulsoap/fetchdata_html.py
+++ beautifulsoap/fetchdata_html.py
@@ -5,30 +5,33 @@ from bs4 import BeautifulSoup
 import os
 import requests
 import re
-import boto
-
 
 DATADIR = ""
 DATAFILE = "awr_rac.html"
 
-# doc = urllib.request.urlopen('http://www.bkzy.org/Index/Declaration?intPageNo=1')
-# doc = doc.read().decode('utf-8')
-#
-# soup = BeautifulSoup(doc, "html.parser")
-
 def extract_data(file):
+    data = []
+    total = 0
     with open(file) as f:
         soup = BeautifulSoup(f,'lxml')
-
-        #awrdata = soup.find(summary="System Statistics - Per Transaction")
         pattern = re.compile(r'^physical')
-        #
-        # for a in soup.find_all("tr",scope='row'):
-        #     if pattern.match(a.text):
-        #         print a.text
+        pattern1 = re.compile(r'total IO requests')
         for rawdata in soup.find_all('table',summary="System Statistics (Global). . per Second Average - average of per-instance per Second rates. per Second Std Dev - standard deviation of per-instance per Second rates. per Second Min - minimum of per-instance per Second rates. per Second Max - maximum of per-instance per Second rates"):
-            for data in rawdata.find_all('td'):
-                print data.text
+            trdatas =  rawdata.find_all(name='tr')
+            for trdata in trdatas:
+                tddatas = BeautifulSoup(str(trdata), "html.parser")
+                tddata = tddatas.find_all(name='td')
+                try:
+                    if  (pattern.match(tddata[0].string)):
+                        data.append({'name':tddata[0].string,'total':float(tddata[1].string.replace(',','')),'per second':float(tddata[2].string.replace(',','')),'per trans':float(tddata[3].string.replace(',',''))})
+                except:
+                    pass
+        print data
+        for i in range(len(data)):
+            if pattern1.search(data[i]["name"]):
+                print data[i]["name"]
+                total += data[i]["total"]
+        print float(total)
 
 
 def fetch():
@@ -36,7 +39,4 @@ def fetch():
     extract_data(awrfile)
 
 if __name__ == "__main__":
-    # conn = boto.connect_s3()
-    # url = conn.generate_url(3600,'GET',bucket='edwintest-s3',key='vagrant and docker command')
-    # print url
     fetch()
