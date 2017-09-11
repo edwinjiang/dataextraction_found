0470644aa6bdb1025af3e9a3abccbcd4ff3bfac7
edwin <986806345@qq.com>
2017-09-11 17:17:26 +0800

    fifthe commit

--- beautifulsoap/splitfile.py
+++ beautifulsoap/splitfile.py
@@ -16,7 +16,6 @@ def split_file(filename):
     Split the input file into separate files, each containing a single patent.
     As a hint - each patent declaration starts with the same line that was
     causing the error found in the previous exercises.
-
     The new files should be saved with filename in the following format:
     "{}-{}".format(filename, n) where n is a counter, starting from 0.
     """
@@ -24,34 +23,41 @@ def split_file(filename):
     end_pos = 0
     start_pos = 0
     current_pos = 0
-    file = open(PATENTS, 'r')
+    file = open(filename, 'r')
     for number, content in enumerate(file):
         if content.startswith("<?xml"):
             xmlnum.append(number)
 
-    for i in range(len(xmlnum) - 1):
+    for i in range(len(xmlnum)):
         splitfile = "{}-{}".format(PATENTS, i)
         start_pos = end_pos
         current_pos = end_pos
+        newfile = open(splitfile, 'w+')
+        print i+1
         with open(PATENTS, 'r') as f:
             for line in f:
-                current_pos += 1
-                end_pos = xmlnum[i + 1]
-                if current_pos >= start_pos and current_pos <= end_pos:
-                    print line
+                if (i + 1) == len(xmlnum):
+                    current_pos += 1
+                    if current_pos >= start_pos and current_pos <= len(open(PATENTS, 'r') .readlines()):
+                        newfile.writelines(line)
+                else:
+                    current_pos += 1
+                    end_pos = xmlnum[i + 1]
+                    if current_pos >= start_pos and current_pos <= end_pos:
+                        newfile.writelines(line)
 
 
 def test():
     split_file(PATENTS)
-    # for n in range(4):
-    #     try:
-    #         fname = "{}-{}".format(PATENTS, n)
-    #         f = open(fname, "r")
-    #         if not f.readline().startswith("<?xml"):
-    #             print "You have not split the file {} in the correct boundary!".format(fname)
-    #         f.close()
-    #     except:
-    #         print "Could not find file {}. Check if the filename is correct!".format(fname)
+    for n in range(4):
+        try:
+            fname = "{}-{}".format(PATENTS, n)
+            f = open(fname, "r")
+            if not f.readline().startswith("<?xml"):
+                print "You have not split the file {} in the correct boundary!".format(fname)
+            f.close()
+        except:
+            print "Could not find file {}. Check if the filename is correct!".format(fname)
 
 
 test()
---
+++ beautifulsoap/validty.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#encoding:utf8
+
+import csv
+import pprint
+
+INPUT_FILE = 'autos.csv'
+OUTPUT_GOOD = 'autos-valid.csv'
+OUTPUT_BAD = 'FIXME-autos.csv'
+
+#skip n lines of CSV file
+# def skip_lines(input_file,skip):
+#     for i in range(0,skip):
+#         next(input_file)
+
+def process_file(input_file, output_good, output_bad):
+    data_good = []
+    data_bad = []
+    with open(input_file, "r") as f:
+        reader = csv.DictReader(f)
+        header = reader.fieldnames
+        for listdata in reader:
+            if listdata['URI'].find('dbpedia.org') > 0:
+                valid_year = listdata['productionStartYear'][:4]
+                try:
+                    valid_year = int(valid_year)
+                    if (valid_year >= 1886) and (valid_year <= 2014):
+                        data_good.append(listdata)
+                    else:
+                        data_bad.append(listdata)
+                except ValueError:
+                    if valid_year == 'NULL':
+                        data_bad.append(listdata)
+
+    with open(OUTPUT_GOOD, "w") as good:
+        writer = csv.DictWriter(good, delimiter=",", fieldnames= header)
+        writer.writeheader()
+        for row in data_good:
+            writer.writerow(row)
+
+    with open(OUTPUT_BAD, "w") as bad:
+        writer = csv.DictWriter(bad, delimiter=",", fieldnames=header)
+        writer.writeheader()
+        for row in data_bad:
+            writer.writerow(row)
+
+def test():
+    process_file(INPUT_FILE, OUTPUT_GOOD, OUTPUT_BAD)
+
+if __name__ == "__main__":
+    test()
